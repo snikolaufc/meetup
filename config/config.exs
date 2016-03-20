ee39392632d08ac8fddda6430abace5f40b09793
Jimmi Fragkos <jimmi.fragkos@gmail.com>
2016-03-20 23:42:30 +0000

    Create dev config file

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,3 @@
 use Mix.Config
-# config :meetup, Meetup.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   database: "meetup",
-#   username: "postgres",
-#   password: "postgres",
-#   hostname: "localhost"
-#   # url: "postgres://postgres:postgres@localhost/meetup"
 
 import_config "#{Mix.env}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,7 @@ use Mix.Config
 
 config :meetup, Meetup.Repo,
   adapter: Ecto.Adapters.Postgres,
-  database: "meetup_test",
+  database: "meetup_dev",
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  hostname: "localhost"
